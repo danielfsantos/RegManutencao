5580fc978bd247c09cca9d8e7e0ce100c15563ce
Daniel Fernandes <danielfsantos56@gmail.com>
2017-08-10 13:22:46 -0300

    Melhoria nas claasses Utilizando a divisão das funções e boas praticas da programação

--- src/application/Estoque.java
+++ src/application/Estoque.java
@@ -1,48 +1,110 @@
 package application;
 
+import javax.swing.JOptionPane;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Estoque extends Application {
 
-	@Override
-	public void start(Stage index) throws Exception {
-		AnchorPane pane = new AnchorPane();
-		pane.setPrefSize(400, 300);
-		Scene cene = new Scene(pane);
+	private TextField txtLogin;
+	private PasswordField txtSenha;
+	private Button btnEntrar;
+	private Button btnSair;
+	private static Stage stage;
+	private AnchorPane pane;
 
-		/* Componentes da Tela */
-		TextField txtLogin = new TextField();
-		TextField txtSenha = new TextField();
-		Button btnEntrar = new Button("Entrar");
-		Button btnSair = new Button("Sair");
+	@Override
+	public void start(Stage stage) throws Exception {
+		initComponents();
+		initListener();
+		Scene cene = new Scene(pane);
+		stage.setScene(cene);
+		stage.setResizable(false);
+		stage.setTitle("LOGIN - RegManutenção");
+		stage.show();
+		initLayout();
+		Estoque.stage = stage;
+	}
+
+	/**
+	 * 
+	 * Inicialização dos componentes
+	 * 
+	 * @author Daniel Fernandes
+	 */
+	public void initComponents() {
+		txtLogin = new TextField();
+		txtLogin.setPromptText("LOGIN");
+		txtSenha = new PasswordField();
+		txtSenha.setPromptText("SENHA");
+		btnEntrar = new Button("ENTRAR");
+		btnSair = new Button("SAIR");
+		pane = new AnchorPane();
+		pane.setPrefSize(400, 300);
+		pane.setStyle("-fx-background-color: linear-gradient(from 0% 0% to 100% 100% , blue 0% , silver 100%);");
 		txtLogin.setPromptText("Input de Login");
 		txtSenha.setPromptText("Txt de Senha");
-
-		/* Configuração dos Componentes da tela */
 		pane.getChildren().addAll(txtLogin, txtSenha, btnEntrar, btnSair);
 
-		index.setScene(cene);
-		index.show();
+	}
 
-		/* Layout dos componentes */
-		txtLogin.setLayoutX(pane.getWidth() - txtLogin.getWidth() / 2);
+	public Stage getStage() {
+		return stage;
+	}
+
+	private void initLayout() {
+		txtLogin.setLayoutX((pane.getWidth() - txtLogin.getWidth()) / 2);
 		txtLogin.setLayoutY(50);
 
-		txtSenha.setLayoutX(pane.getWidth() - txtSenha.getWidth() / 2);
+		txtSenha.setLayoutX((pane.getWidth() - txtSenha.getWidth()) / 2);
 		txtSenha.setLayoutY(100);
 
-		btnEntrar.setLayoutX(pane.getWidth() - btnEntrar.getWidth() / 2);
+		btnEntrar.setLayoutX((pane.getWidth() - btnEntrar.getWidth()) / 2);
 		btnEntrar.setLayoutY(150);
 
-		btnSair.setLayoutX(pane.getWidth() - btnSair.getWidth() / 2);
+		btnSair.setLayoutX((pane.getWidth() - btnSair.getWidth()) / 2);
 		btnSair.setLayoutY(200);
+	}
 
-		pane.setStyle("-fx-background-color: linear-gradient(from 0% 0% to 100% 100% , blue 0% , silver 100%);");
+	private void initListener() {
+		btnSair.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				fecharAplicativo();
+
+			}
+		});
+
+		btnEntrar.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				entrar();
+
+			}
+		});
+
+	}
+
+	public void fecharAplicativo() {
+		System.exit(0);
+	}
+
+	public void entrar() {
+		if (txtLogin.getText().equals("Daniel") && txtSenha.getText().equals("123")) {
+
+		} else {
+			JOptionPane.showMessageDialog(null, "DEU ERRO", "ERROR", JOptionPane.ERROR_MESSAGE);
+		}
 	}
 
 	public static void main(String[] args) {
